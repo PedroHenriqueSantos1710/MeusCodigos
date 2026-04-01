3ecb216f63b0a9bbaa8290db480661b72cb75ee7
PEDRO HENRIQUE SANTOS LIMA <pedrolima005@cs.cruzeirodosul.edu.br>
2026-04-01 19:36:39 -0300

    v2

--- votar.java
+++ votar.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class votar {
     public static void main(String[] args) {
-      
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Digite Sua Idade: ");
+      int idade = sc.nextInt();
+
+      if(idade >= 18){
+        System.out.println("Maior de idade! Pode votar!");
+      }else{
+        System.out.println("Menor de idade! Não pode votar!");
+      }
         
-        
+
     }
 }
